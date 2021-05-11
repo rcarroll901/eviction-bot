3184f9de3cdc896c50f0bad6885bd5b0d416eebe
Ryan Carroll <raec901@gmail.com>
2021-05-10 20:03:26 -0500

    change data source and target to Airtable

---
+++ evict_tools/persist.py
@@ -0,0 +1,24 @@
+import os
+import sys
+sys.path.append('/opt')
+sys.path.append('package')
+sys.path.append('package/bin')
+
+from airtable import Airtable
+
+# get Airtable 
+base_key = os.environ['AT_BASE_KEY']
+table_name = os.environ['AT_TABLE_NAME']
+api_key = os.environ['AT_API_KEY']
+airtable = Airtable(base_key=base_key, table_name=table_name, api_key=api_key)
+
+
+def get_case_ids():
+    # read in all eviction record_id + case numbers
+    response = airtable.get_all(fields=['Eviction Case Number'], formula='{Eviction Case Number}!=""')
+    return [{'record_id': r['id'], 'case_id': r['fields']['Eviction Case Number'][0]} for r in response]
+
+def update_row(record_id, scrape_dict):
+    airtable.update(record_id, scrape_dict)
+    return "Success"
+
